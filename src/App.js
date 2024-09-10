c3e5cae2e1516d3658ac13d1a51b86b72c1cc201
Hokage <dertarus3222@gmail.com>
2024-09-10 08:55:04 +0500

    init

--- src/App.js
+++ src/App.js
@@ -1,25 +1,47 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import Header from "./components/Header";
+import Footer from "./components/Footer";
+import News from "./components/News";
+import Catalog from "./components/Catalog";
+import Delivery from "./components/Delivery";
+import Contacts from "./components/Contacts";
+import OrderForm from "./components/OrderForm";
+import "./App.css";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div className="app">
+        <Header />
+        <main>
+          <Routes>
+            <Route
+              path="/"
+              element={
+                <>
+                  <section className="section">
+                    <News />
+                  </section>
+                  <section className="section">
+                    <Catalog />
+                  </section>
+                  <section className="section">
+                    <Delivery />
+                  </section>
+                  <section className="section">
+                    <Contacts />
+                  </section>
+                </>
+              }
+            />
+            <Route path="/order" element={<OrderForm />} />
+          </Routes>
+        </main>
+      </div>
+      <Footer />
+    </Router>
   );
-}
+};
 
 export default App;
